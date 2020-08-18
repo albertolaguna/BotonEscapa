04aeb2abb6fdf4e6c1dbd5ce2e14754e80fb47d3
Alberto <Alberto@laguna.mshome.net>
2020-08-18 10:48:03 -0500

    V1.0

---
+++ suscribirse/src/main/java/com/tiktok/jalagunam/suscribirse/App.java
@@ -0,0 +1,10 @@
+package com.tiktok.jalagunam.suscribirse;
+
+import com.tiktok.jalagunam.gui.PrincipalWindow;
+
+public class App {
+	public static void main(String[] args) {
+		PrincipalWindow window = new PrincipalWindow();
+		window.setVisible(true);
+	}
+}
